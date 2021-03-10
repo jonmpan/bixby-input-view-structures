933ebe42284a7679e5ac0d5690413e73b2bb1547
jonmpan <jonmpan@gmail.com>
2021-03-10 08:39:13 -0800

    first commit

---
+++ playground.inputviewstructure/code/GetSearchTerms.js
@@ -0,0 +1,8 @@
+module.exports.function = function getSearchTerms() {
+  var searchTerms = [
+    { searchTerm: "Cat", icon: "/images/icons/cat.png", description: "Meowtastically cute" },
+    { searchTerm: "Dog", icon: "/images/icons/dog.png", description: "Bundles of energy and joy" },
+    { searchTerm: "Other", icon: "/images/icons/other.png", description: "Other animals are cute too!" },
+  ];
+  return searchTerms
+}
---
+++ playground.inputviewstructure/code/content.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    "text": "Unlike humans who have 206 bones while cats have a range of 230-250 bones.",
+    "image": {
+      "url": "images/moxie.jpg"
+    },
+    "tags": [
+      "cat"
+    ]
+  },
+  {
+    "text": "A house cat is genetically 95.6% tiger.",
+    "image": {
+      "url": "images/moxie-3.png"
+    },
+    "tags": [
+      "cat"
+    ]
+  },
+  {
+    "text": "Did you know that dogs have three eyelids?",
+    "tags": [
+      "dog"
+    ]
+  },
+  {
+    "text": "Bunnies and rabbits are actually the same thing.",
+    "tags": [
+      "other"
+    ]
+  }
+]
